2fec9646db5a359f8d32c692c44a6507c5cfcd57
Avadh K Sapariya <avadhksapariya.ak56du@gmail.com>
2024-01-01 21:18:24 +0530

    Homepage working done.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController valueController = TextEditingController();
+  double result = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,10 +17,40 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Currency Converter'),
       ),
-      body: const Center(
-        child: Text('Hello world !'),
+      body: Container(
+        color: Theme.of(context).copyWith().highlightColor,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                result.toString(),
+                style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                controller: valueController,
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'Please enter value in USD',
+                  suffixIcon: Icon(Icons.monetization_on_outlined),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = double.parse(valueController.text) * 81;
+                  });
+                },
+                child: const Text('Convert'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-
 }
